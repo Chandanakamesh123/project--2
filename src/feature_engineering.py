c7d9ef5529b603c2752f3a95f106643ac87d3d66
Chandanakamesh123 <chandanak.cse2024@citchennai.net>
2026-05-15 18:43:30 +0530

    Create feature_engineering.py

---
+++ src/feature_engineering.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv('dataset/processed_shipping_data.csv')
+
+# Convert dates
+df['Scheduled_Delivery_Date'] = pd.to_datetime(
+    df['Scheduled_Delivery_Date']
+)
+
+df['Actual_Delivery_Date'] = pd.to_datetime(
+    df['Actual_Delivery_Date']
+)
+
+# Create Delay_Days column
+df['Delay_Days'] = (
+    df['Actual_Delivery_Date']
+    - df['Scheduled_Delivery_Date']
+).dt.days
+
+# Create Efficiency Score
+df['Efficiency_Score'] = 100 - (df['Delay_Days'] * 10)
+
+# Save updated dataset
+df.to_csv('dataset/processed_shipping_data.csv', index=False)
+
+print("Feature engineering completed")
