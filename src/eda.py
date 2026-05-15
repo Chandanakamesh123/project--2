b815a442175e7f6f31fa77caab02e116f5f76643
Chandanakamesh123 <chandanak.cse2024@citchennai.net>
2026-05-15 18:42:43 +0530

    Create eda.py

---
+++ src/eda.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load processed dataset
+df = pd.read_csv('dataset/processed_shipping_data.csv')
+
+# Shipment Mode Analysis
+print(df['Shipment_Mode'].value_counts())
+
+# Vendor Analysis
+print(df['Vendor'].value_counts())
+
+# Delivery Status Analysis
+print(df['Delivery_Status'].value_counts())
+
+# Shipment Mode Bar Chart
+df['Shipment_Mode'].value_counts().plot(kind='bar')
+
+plt.title("Shipment Mode Analysis")
+
+plt.xlabel("Shipment Mode")
+
+plt.ylabel("Count")
+
+plt.show()
