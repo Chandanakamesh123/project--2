1cb88b307fd9a323c0f4be58b86ebfab01f40753
Chandanakamesh123 <chandanak.cse2024@citchennai.net>
2026-05-16 07:47:58 +0530

    Update data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load dataset
-df = pd.read_csv('dataset/raw_shipping_data.csv')
+df = pd.read_csv('raw_shipping_data.csv')
 
 # Remove missing values
 df.dropna(inplace=True)
@@ -19,6 +19,6 @@ df['Actual_Delivery_Date'] = pd.to_datetime(
 )
 
 # Save cleaned dataset
-df.to_csv('dataset/processed_shipping_data.csv', index=False)
+df.to_csv('processed_shipping_data.csv', index=False)
 
 print("Data cleaning completed successfully")
